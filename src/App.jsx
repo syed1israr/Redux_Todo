3967c6ad6e8a97fde6fc2a49f291abff2137a397
syedFaraz <syedfaraz173@gmail.com>
2023-11-16 16:09:28 +0530

    Best For Revising Redux and Change Themes later my Future Self :)

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Learn about redux toolkit 😊💕</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
